a133ae97f476f250834f7991cf17ce2883c467e9
kamila <czajkowska.kamila@wp.pl>
2023-08-02 17:45:32 +0200

    Modified views, url-  possibility to check the posts

--- cookMeAgainProject/cookMe/urls.py
+++ cookMeAgainProject/cookMe/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('postRecipe/', views.postRecipe, name='postRecipe'),
 ]
--- cookMeAgainProject/cookMe/views.py
+++ cookMeAgainProject/cookMe/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Recipe
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def postRecipe(request):
+    postRecipes = Recipe.objects.all().order_by('-createDate') # objects - fetches all objects from the database
+    return render(request, 'postRecipe.html', {'post':postRecipes})
